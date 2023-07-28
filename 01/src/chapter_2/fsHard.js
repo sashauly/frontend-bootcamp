f236ef2a0fc62e2ab4782fd3d25ed75c5572b545
sashauly <sashauly.code@gmail.com>
2023-07-28 23:16:45 +0300

    add 01 day

--- 01/src/chapter_2/fsHard.js
+++ 01/src/chapter_2/fsHard.js
@@ -1,3 +1,49 @@
-const fs = require('fs');
+import { readdir, stat } from 'fs/promises';
+import path from 'path';
+import cliProgress from 'cli-progress';
+// Тебе предстоит написать progressbar, применимый не к статически заданному времени,
+// eslint-disable-next-line max-len
+// а к размеру "загружаемых" файлов. На основании полученых размеров файлов, реализуй свой progressbar.
 
-export default const progressbar = async () => {};
+// Твой прогрессбар будет заполняться до общего размера всех имеющихся файлов в папке.
+// Если у тебя есть файл, размером 5Кб при общем размере всех файлов - 10 Кб,
+// eslint-disable-next-line max-len
+// то твоя полоса загрузки после обработки такого файла должна заполниться на 50% (100% * (5Кб / 10Кб)).
+
+// Сделай полосу загрузки(progressbar) в Консоли Терминала. Не в браузере!
+// Она должна увеличиваться с каждым новым загруженным файлом
+
+const progressbar = async () => {
+  const folderPath = './files/fsHard';
+
+  let totalSize = 0;
+  const arrayOfFiles = [];
+  const files = await readdir(folderPath);
+
+  await Promise.all(
+    files.map(async (file) => {
+      const stats = await stat(path.join(folderPath, '/', file));
+      arrayOfFiles.push(stats.size);
+      totalSize += stats.size;
+    }),
+  );
+
+  const progressBar = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic,
+  );
+
+  progressBar.start(totalSize, 0);
+  let currentSize = 0;
+  for (const fileSize of arrayOfFiles) {
+    currentSize += fileSize;
+    progressBar.update(currentSize);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  }
+
+  progressBar.stop();
+};
+
+await progressbar();
+
+export default progressbar;
--- 01/src/chapter_2/fsSimple.js
+++ 01/src/chapter_2/fsSimple.js
@@ -1,13 +1,52 @@
-const fs = requere("fs");
+import { promises, readFile, writeFile } from 'fs';
 
-const readAndWriteCallbackHell = () => {};
+// eslint-disable-next-line max-len
+// Вам нужно прочитать данные из файла file1.txt, после прочтения полученные данные надо записать в файл file2.txt
+// Реализовать это надо 3 разными способами, для каждого способа своя функция:
+// eslint-disable-next-line max-len
+// - readAndWriteCallbackHell() - в данной функции вы должны использовать только передачу коллбека в ассинхронную функцию.
+// - readAndWritePromises() - в данной функции вы должны использовать промисы и then.
+// - readAndWriteAsyncAwait() - в данной функции можно использовать async await.
 
-const readAndWritePromises = () => {};
+const readAndWriteCallbackHell = () => {
+  readFile('./files/fsSimple/file1.txt', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    writeFile('./files/fsSimple/file2.txt', data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('CallBack Hell SUCCESS!');
+    });
+  });
+};
 
-const readAndWriteAsyncAwait = async () => {};
+const readAndWritePromises = () => {
+  promises
+    .readFile('./files/fsSimple/file1.txt', 'utf-8')
+    .then((data) => promises.writeFile('./files/fsSimple/file2.txt', data))
+    .then(() => console.log('.then Promises SUCCESS!'))
+    .catch((err) => console.error(err));
+};
 
-export {
+const readAndWriteAsyncAwait = async () => {
+  try {
+    const data = await promises.readFile('./files/fsSimple/file1.txt', 'utf-8');
+    await promises.writeFile('./files/fsSimple/file2.txt', data);
+    console.log('async await SUCCESS!');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export default {
   readAndWriteAsyncAwait,
   readAndWritePromises,
   readAndWriteCallbackHell,
 };
+readAndWriteAsyncAwait();
+readAndWritePromises();
+readAndWriteCallbackHell();
