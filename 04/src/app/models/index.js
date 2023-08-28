97283d659e6a60e3d0fb94532f6cfcc3867d7d76
sashauly <sashauly.code@gmail.com>
2023-08-28 21:42:47 +0300

    add smthing

--- 04/src/app/models/index.js
+++ 04/src/app/models/index.js
@@ -1,40 +1,50 @@
+import fs from 'fs';
 import Sequelize from 'sequelize';
-import {
-  HOST,
-  USER,
-  PASSWORD,
-  DB,
-  dialect as _dialect,
-  pool as _pool,
-} from '../config/db.config.js';
+import path from 'path';
+import process from 'process';
+import { fileURLToPath } from 'url';
 
-const sequelize = new Sequelize(DB, USER, PASSWORD, {
-  host: HOST,
-  dialect: _dialect,
-  operatorsAliases: 0,
+// const { config } = JSON.parse(fs.readFileSync('../config/config.json'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+const configPath = path.join(__dirname, '..', 'config', 'config.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+console.log(basename);
 
-  pool: {
-    max: _pool.max,
-    min: _pool.min,
-    acquire: _pool.acquire,
-    idle: _pool.idle,
-  },
-});
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+  }
+);
 
 const db = {};
-db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-db.tutorials = db.sequelize.define('tutorial', {
-  title: {
-    type: db.Sequelize.STRING,
-  },
-  description: {
-    type: db.Sequelize.STRING,
-  },
-  published: {
-    type: db.Sequelize.BOOLEAN,
-  },
-});
+// fs.readdirSync(__dirname)
+//   .filter(
+//     (file) =>
+//       file.indexOf('.') !== 0 &&
+//       file !== basename &&
+//       file.slice(-3) === '.js' &&
+//       file.indexOf('.test.js') === -1
+//   )
+//   .forEach((file) => {
+//     const model = require(path.join(__dirname, file))(
+//       sequelize,
+//       Sequelize.DataTypes
+//     );
+//     db[model.name] = model;
+//   });
 
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
 export default db;
